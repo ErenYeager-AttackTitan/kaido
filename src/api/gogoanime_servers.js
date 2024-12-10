d050c6602737b7f23bb8268c8578e4cdb12fdd11
ErenYeager-AttackTitan <eren.yeager.founding.titan.9@gmail.com>
2024-12-10 16:52:41 +0530

    Update gogoanime_servers.js

--- src/api/gogoanime_servers.js
+++ src/api/gogoanime_servers.js
@@ -1,6 +1,6 @@
 export const servers = [
+  { id: "vidstreaming", name: "Vidstreaming" },
   { id: "gogocdn", name: "Gogo server" },
   { id: "streamwish", name: "Streamwish" },
-  { id: "vidstreaming", name: "Vidstreaming" },
   { id: "mp4upload", name: "Mp4Upload" },
 ];
